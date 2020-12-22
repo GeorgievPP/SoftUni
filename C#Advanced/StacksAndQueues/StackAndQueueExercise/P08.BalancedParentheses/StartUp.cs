1537705c5ec2c839c553909c7eb59fbf56aff9e4
calevra1807@abv.bg <calevra1807@abv.bg>
2020-12-22 23:32:52 +0200

    StackAndQueueExeProblems

---
+++ C#Advanced/StacksAndQueues/StackAndQueueExercise/P08.BalancedParentheses/StartUp.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08.BalancedParentheses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<char> queue = new Queue<char>(input);
+
+            int count = 0;
+
+            bool isBalanced = true;
+
+            if(queue.Count % 2 != 0)
+            {
+                Console.WriteLine("NO");
+
+                return;
+            }
+
+            while(queue.Any())
+            {
+                char firstChar = queue.Dequeue();
+
+                char secondChar = queue.Peek();
+
+                if(firstChar == '{')
+                {
+
+                    if(secondChar == '}')
+                    {
+                        queue.Dequeue();
+
+                        count = 0;
+
+                        continue;
+
+                    }
+                    else
+                    {
+
+                        queue.Enqueue(firstChar);
+
+                    }
+
+                }
+
+                else if(firstChar == '[')
+                {
+                    if(secondChar == ']')
+                    {
+
+                        queue.Dequeue();
+
+                        count = 0;
+
+                        continue;
+
+                    }
+                    else
+                    {
+
+                        queue.Enqueue(firstChar);
+
+                    }
+
+                }
+
+                else if(firstChar == '(')
+                {
+
+                    if(secondChar == ')')
+                    {
+
+                        queue.Dequeue();
+
+                        count = 0;
+
+                        continue;
+                    }
+                    else
+                    {
+                        queue.Enqueue(firstChar);
+                    }
+                }
+
+                else
+                {
+
+                    queue.Enqueue(firstChar);
+
+                }
+
+                count++;
+
+                if(count == queue.Count)
+                {
+
+                    isBalanced = false;
+
+                    break;
+
+                }
+
+            }
+
+            if(isBalanced)
+            {
+
+                Console.WriteLine("YES");
+
+            }
+            else
+            {
+
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ C#Advanced/StacksAndQueues/StackAndQueueExercise/P09.SimpleTextEditor/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P09.SimpleTextEditor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push(sb.ToString());
+
+            for(int i = 0; i < n; i++)
+            {
+
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = input[0];
+
+                if(command == "1")
+                {
+
+                    sb.Append(input[1]);
+
+                    stack.Push(sb.ToString());
+
+                    continue;
+                }
+
+                else if(command == "2")
+                {
+
+                    int count = int.Parse(input[1]);
+
+                    sb.Remove(sb.Length - count, count);
+
+                    stack.Push(sb.ToString());
+
+                    continue;
+
+                }
+
+                else if(command == "3")
+                {
+                    int index = int.Parse(input[1]);
+
+                    Console.WriteLine(sb[index - 1]);
+
+                    continue;
+                }
+                else if(command == "4")
+                {
+
+                    stack.Pop();
+
+                    sb = new StringBuilder();
+
+                    sb.Append(stack.Peek());
+
+                    continue;
+                }
+
+            }
+        }
+    }
+}
