f2498b73b7596703f9cf13c98f6e0237256c7e87
calevra1807@abv.bg <calevra1807@abv.bg>
2020-12-24 23:48:33 +0200

    MultidimensionalArraysExeProblems

---
+++ C#Advanced/MultidimensionalArrays/MultidimensionalArraysExe/P01.DiagonalDifference/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.PrimaryDiagonal
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            matrix = FillMatrix(n);
+
+            int absDifference = 0;
+
+            absDifference = GetDiagonalDifference(matrix);
+
+            Console.WriteLine(absDifference);
+
+
+        }
+
+        private static int[,] FillMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    matrix[row, col] = rowData[col];
+
+                }
+
+            }
+
+            return matrix;
+
+        }
+
+        private static int GetDiagonalDifference(int[,] matrix)
+        {
+
+            int sumPrimDiagonal = 0;
+
+            int sumSecDiagonal = 0;
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    if (row == col)
+                    {
+
+                        sumPrimDiagonal += matrix[row, col];
+
+                    }
+
+                    if(col == matrix.GetLength(1) - 1 - row)
+                    {
+
+                        sumSecDiagonal += matrix[row, col];
+
+                    }
+                }
+            }
+
+            sum = Math.Abs(sumPrimDiagonal - sumSecDiagonal);
+
+            return sum;
+
+        }
+    }
+}
---
+++ C#Advanced/MultidimensionalArrays/MultidimensionalArraysExe/P02.SquaresInMatrix2x2/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace P02._2x2SquaresInMatrix
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixInfo = Console.ReadLine()
+                .Split().Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixInfo[0];
+
+            int cols = matrixInfo[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            FillMatrix(matrix);
+
+            int count = 0;
+
+            PrintCount(rows, cols, matrix, count);
+
+        }
+
+        private static void PrintCount(int rows, int cols, char[,] matrix, int count)
+        {
+            for (int row = 0; row < rows - 1; row++)
+            {
+
+                for (int col = 0; col < cols - 1; col++)
+                {
+
+                    char currElement = matrix[row, col];
+
+                    if (currElement == matrix[row, col + 1]
+                        && currElement == matrix[row + 1, col]
+                        && currElement == matrix[row + 1, col + 1])
+                    {
+
+                        count++;
+
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void FillMatrix(char[,] matrix)
+        {
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                char[] currentRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+
+                }
+            }
+
+        }
+
+    }
+}
