316a20f6dfee9392c26a501d172b340422c661b3
calevra1807@abv.bg <calevra1807@abv.bg>
2020-12-29 23:18:30 +0200

    SetProblems

---
+++ C#Advanced/SetsAndDictionarys/Lab/P06.ParkingLot/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.ParkingLot
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            HashSet<string> parking = new HashSet<string>();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+
+                string[] inputInfo = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = inputInfo[0].ToLower();
+
+                string carNumber = inputInfo[1];
+
+                if (direction == "in")
+                {
+
+                    parking.Add(carNumber);
+
+                }
+
+                else
+                {
+
+                    parking.Remove(carNumber);
+
+                }
+
+            }
+
+            if (parking.Count == 0)
+            {
+
+                Console.WriteLine("Parking Lot is Empty");
+
+            }
+
+            else
+            {
+
+                foreach (var car in parking)
+                {
+
+                    Console.WriteLine(car);
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ C#Advanced/SetsAndDictionarys/Lab/P07.SoftUniParty/StartUp.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07.SoftUniParty
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<string> vipSet = new HashSet<string>();
+
+            HashSet<string> regularSet = new HashSet<string>();
+
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+
+                if (input == "PARTY")
+                {
+
+                    while (input != "END")
+                    {
+
+                        input = Console.ReadLine();
+
+                        if (char.IsDigit(input[0]))
+                        {
+                            vipSet.Remove(input);
+
+                        }
+
+                        else
+                        {
+
+                            regularSet.Remove(input);
+                        }
+
+                    }
+
+                    break;
+
+                }
+
+                else
+                {
+                    if (char.IsDigit(input[0]))
+                    {
+                        vipSet.Add(input);
+
+                    }
+
+                    else
+                    {
+
+                        regularSet.Add(input);
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine(vipSet.Count + regularSet.Count);
+
+            if (vipSet.Count > 0)
+            {
+
+                foreach (var number in vipSet)
+                {
+
+                    Console.WriteLine(number);
+
+                }
+
+            }
+
+            if (regularSet.Count > 0)
+            {
+
+                foreach (var number in regularSet)
+                {
+
+                    Console.WriteLine(number);
+
+                }
+
+            }
+
+        }
+    }
+}
