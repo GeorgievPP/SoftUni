0b5eb47a951110ea857e55937852d75c51b9b745
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-06 11:13:20 +0200

    demoProb

---
+++ C#Advanced/New folder/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Car car = new Car();
+
+            car.Make = "Nissan";
+
+            car.Model = "X-Trail";
+
+            car.Year = 2005;
+
+            Console.WriteLine($"Make: {car.Make} - {car.Model} - {car.Year}");
+        }
+    }
+}
