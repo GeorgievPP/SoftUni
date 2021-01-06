2dd0072b2cb4e381dea08588430b41265e91c1c5
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-06 13:04:11 +0200

    SimpleClassesProblems

---
+++ C#Advanced/DefiningClasses/Lab/P02.CarManufacturer/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Car car = new Car();
+
+            car.Make = "Nissan";
+
+            car.Model = "X-Trail";
+
+            car.Year = 2005;
+
+            // Console.WriteLine($"Make: {car.Make} - {car.Model} - {car.Year}");
+
+            car.FuelConsumption = 8;
+
+            car.FuelQuantity = 60;
+
+            car.Drive(4);
+
+            car.Drive(10);
+
+            Console.WriteLine(car.WhoAmI());
+            
+        }
+    }
+}
