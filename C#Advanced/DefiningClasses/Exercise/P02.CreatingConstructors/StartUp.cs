1d7e4e15befa51f30357f14444f32945f9715adf
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-07 10:15:43 +0200

    SimpleClassesProblems

---
+++ C#Advanced/DefiningClasses/Exercise/P02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Pesho";
+
+            int age = 24;
+
+            Person pesho = new Person(age, name);
+
+            Console.WriteLine($"{pesho.Name} -> {pesho.Age}");
+        }
+    }
+}
