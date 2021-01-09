527652b712b69f415932f1c59dde55cf350a70dc
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-09 13:05:13 +0200

    SimpleClassesProblems

---
+++ C#Advanced/DefiningClasses/Exercise/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.DateModifier
+{
+    public class DateModifier
+    {
+
+        public int GetDifferenceOfTwoDates(string dateOne, string dateTwo)
+        {
+
+
+            int[] dateOneInfo = dateOne
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            int year1 = dateOneInfo[0];
+
+            int month1 = dateOneInfo[1];
+
+            int day1 = dateOneInfo[2];
+
+            DateTime dateTime1 = new DateTime(year1, month1, day1);
+
+
+
+            int[] dateTwoInfo = dateTwo
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            int year2 = dateTwoInfo[0];
+
+            int month2 = dateTwoInfo[1];
+
+            int day2 = dateTwoInfo[2];
+
+            DateTime dateTime2 = new DateTime(year2, month2, day2);
+
+            return Math.Abs((dateTime1 - dateTime2).Days);
+        }
+    }
+}
---
+++ C#Advanced/DefiningClasses/Exercise/P05.DateModifier/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P05.DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dateOne = Console.ReadLine();
+
+            string dateTwo = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            Console.WriteLine(dateModifier.GetDifferenceOfTwoDates(dateOne, dateTwo));
+        }
+    }
+}
