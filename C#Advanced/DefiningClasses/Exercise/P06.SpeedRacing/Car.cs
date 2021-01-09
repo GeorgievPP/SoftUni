a898e77ab8283a04e8120ac8377997a94161e55a
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-09 12:27:32 +0200

    SimpleClassesProblems

---
+++ C#Advanced/DefiningClasses/Exercise/P06.SpeedRacing/Car.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+
+namespace P06.SpeedRacing
+{
+     public class Car
+    {
+
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKm;
+        private double travelledDistance;
+
+
+        public Car (string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+            this.TravelledDistance = 0;
+
+        }
+
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+
+        }
+
+        public double FuelAmount
+        {
+
+            get
+            {
+                return this.fuelAmount;
+            }
+            set
+            {
+                this.fuelAmount = value;
+            }
+
+        }
+
+        public double FuelConsumptionPerKm
+        {
+
+            get
+            {
+                return this.fuelConsumptionPerKm;
+            }
+            set
+            {
+                this.fuelConsumptionPerKm = value;
+            }
+
+        }
+
+
+        public double TravelledDistance
+        {
+
+            get
+            {
+                return this.travelledDistance;
+            }
+            set
+            {
+                this.travelledDistance = value;
+            }
+
+        }
+
+
+
+        public void Drive( string model, double amountOfKm)
+        {
+
+            double targetLitters = amountOfKm * this.FuelConsumptionPerKm;
+
+            if(targetLitters > this.FuelAmount)
+            {
+
+                Console.WriteLine("Insufficient fuel for the drive");
+
+            }
+
+            else
+            {
+
+                this.FuelAmount -= targetLitters;
+
+                this.TravelledDistance += amountOfKm;
+
+            }
+        }
+
+
+        public override string ToString()
+        {
+
+            return $"{this.Model} {this.FuelAmount:f2} {this.TravelledDistance}";
+
+        }
+    }
+}
---
+++ C#Advanced/DefiningClasses/Exercise/P06.SpeedRacing/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06.SpeedRacing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<Car> cars = new HashSet<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] carInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = carInfo[0];
+
+                double fuelAmount = double.Parse(carInfo[1]);
+
+                double fuelConsumptionPerKm = double.Parse(carInfo[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKm);
+
+                cars.Add(car);
+
+            }
+
+            string command;
+
+            while((command = Console.ReadLine()) != "End")
+            {
+
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = cmdArgs[1];
+
+                double amountOfKm = double.Parse(cmdArgs[2]);
+
+                Car currentCar = cars
+                    .Where(c => c.Model == model)
+                    .ToHashSet()
+                    .First();
+
+
+                currentCar.Drive(model, amountOfKm);
+
+            }
+
+            foreach(Car car in cars)
+            {
+
+                Console.WriteLine(car);
+
+            }
+        }
+    }
+}
