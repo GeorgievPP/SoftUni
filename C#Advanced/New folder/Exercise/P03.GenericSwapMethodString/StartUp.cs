96cae505dd818aa118f2b0b61317cc9caed75392
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-11 22:22:16 +0200

    GenericProblems

---
+++ C#Advanced/New folder/Exercise/P03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string name =Console.ReadLine();
+
+                names.Add(name);
+
+            }
+                Box<string> box = new Box<string>(names);
+
+            int[] indexToSwap = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            box.Swap(box.Values, indexToSwap[0], indexToSwap[1]);
+
+            Console.WriteLine(box);
+
+        }
+    }
+}
