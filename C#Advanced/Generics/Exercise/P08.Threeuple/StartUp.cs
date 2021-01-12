3a24dd4f80e22f80d28b82c2da1ddc60f1b94eb2
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-12 15:25:32 +0200

    GenericProblems

---
+++ C#Advanced/Generics/Exercise/P08.Threeuple/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var personInfo = Console.ReadLine().Split(" ", 4).ToArray();
+
+            string fullName = $"{personInfo[0]} {personInfo[1]}";
+
+            string address = $"{personInfo[2]}";
+
+            string town = $"{personInfo[3]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+
+            string name = nameAndBeer[0];
+
+            int beerAmount = int.Parse(nameAndBeer[1]);
+
+            bool drunk = nameAndBeer[2] == "drunk";
+
+            var thirdInput = Console.ReadLine().Split();
+
+            var firstArgument = thirdInput[0];
+
+            var secondArgument = double.Parse(thirdInput[1]);
+
+            var tirdArgument = thirdInput[2];
+
+            Tuple<string, string, string> firstTuple = new Tuple<string, string, string>(fullName, address, town);
+
+            Tuple<string, int, bool> secondTuple = new Tuple<string, int, bool>(name, beerAmount, drunk);
+
+            Tuple<string, double, string> thirdTuple = new Tuple<string, double, string>(firstArgument, secondArgument, tirdArgument);
+
+            Console.WriteLine(firstTuple);
+
+            Console.WriteLine(secondTuple);
+
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C#Advanced/Generics/Exercise/P08.Threeuple/Tuple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<TFirst, TSecond, TTird>
+    {
+
+        public Tuple(TFirst firstItem, TSecond secondItem, TTird tirdItem)
+        {
+
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+            this.TirdItem = tirdItem;
+
+        }
+
+        public TFirst FirstItem { get; set; }
+
+        public TSecond SecondItem { get; set; }
+
+        public TTird TirdItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem} -> {this.TirdItem}";
+
+        }
+    }
+
+}
