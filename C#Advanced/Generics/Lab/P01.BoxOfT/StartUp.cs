33d07c1d7496dad3eb4ef94e462e499cc6fb5e70
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-12 10:30:08 +0200

    GenericProblems

---
+++ C#Advanced/Generics/Lab/P01.BoxOfT/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var box = new Box<int>();
+
+            box.Add(10);
+            box.Add(20);
+            box.Add(40);
+
+            Console.WriteLine(box.Remove());
+
+            box.Add(4);
+            box.Add(6);
+
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
