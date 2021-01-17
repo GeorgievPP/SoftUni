be0cbd29efda77d1a6f23921ec5dcc870334c74c
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-17 20:48:51 +0200

    examProblems

---
+++ C#Advanced/ExamPractice/P01.Sheduling/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Sheduling
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tasksInput = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> tasks = new Stack<int>(tasksInput);
+
+
+            int[] threadsInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> threads = new Queue<int>(threadsInput);
+
+
+            int valueOfTask = int.Parse(Console.ReadLine());
+
+            int killerOfTask = 0;
+            string threadsResult = "";
+
+            while (true)
+            {
+
+                int currentTask = tasks.Peek();
+                int currentThread = threads.Peek();
+
+                if (currentTask == valueOfTask)
+                {
+
+                    killerOfTask = currentThread;
+
+                    threadsResult = String.Join(" ", threads);
+
+                    break;
+
+                }
+
+                if (currentThread >= currentTask)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+
+                    continue;
+
+                }
+
+                else
+                {
+                    threads.Dequeue();
+
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine($"Thread with value {killerOfTask} killed task {valueOfTask}");
+
+            Console.WriteLine(threadsResult);
+
+        }
+    }
+}
---
+++ C#Advanced/ExamPractice/P02.Garden/StartUp.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace P02.Garden
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int[] matrixSize = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            if (rows == cols)
+            {
+
+                int[,] matrix = new int[rows, cols];
+
+                FillGarden(matrix);
+
+                string input;
+
+                while ((input = Console.ReadLine()) != "Bloom Bloom Plow")
+                {
+                    int[] plantFlowerPos = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse).ToArray();
+
+                    int plantFlowerPosRow = plantFlowerPos[0];
+                    int plantFlowerPosCol = plantFlowerPos[1];
+
+                    if (plantFlowerPosRow < 0 || plantFlowerPosRow >= matrix.GetLength(0)
+                        || plantFlowerPosCol < 0 || plantFlowerPosCol >= matrix.GetLength(1))
+                    {
+                        Console.WriteLine("Invalid coordinates.");
+                        continue;
+                    }
+
+                    PlantingGarden(matrix, plantFlowerPos);
+                }
+
+                PrintGarden(matrix);
+            }
+
+        }
+
+        private static void PlantingGarden(int[,] matrix, int[] input)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == input[0] || col == input[1])
+                    {
+
+                        matrix[row, col] += 1;
+                    }
+
+                }
+
+            }
+
+        }
+
+        public static void FillGarden(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+
+        }
+
+        public static void PrintGarden(int[,] matrix)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sb.Append(matrix[row, col]);
+
+                    if (col != matrix.GetLength(1) - 1)
+                    {
+                        sb.Append(" ");
+
+                    }
+                }
+
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
