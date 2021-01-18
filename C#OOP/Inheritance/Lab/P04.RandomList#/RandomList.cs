2ee09f2d1adcfcfaa18fc5335dfba0aa2d96bb19
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-18 09:56:04 +0200

    InheritanceProblems

---
+++ C#OOP/Inheritance/Lab/P04.RandomList#/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+
+namespace CustomRandomList
+{
+
+    public class RandomList : ArrayList
+    {
+
+        private Random random;
+
+        public RandomList()
+        { 
+            this.random = new Random();
+        }
+
+        public int RandomInt()
+        {
+            return this.random.Next();
+        }
+
+
+        public int RandomString() =>  this.RandomInt();
+
+    }
+}
