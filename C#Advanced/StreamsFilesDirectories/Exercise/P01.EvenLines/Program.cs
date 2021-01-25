6f3fd927962658cda4d64eaf0393c0e62b27eaae
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-25 19:52:23 +0200

    StreamsFilesDirecProblems

---
+++ C#Advanced/StreamsFilesDirectories/Exercise/P01.EvenLines/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace P01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+
+               // string[] lineArr = line.Split();
+                 int counter = 0;
+
+                using(StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while(line != null)
+                    {
+                        string[] lineArr = line.Split();
+                      
+                        if (counter % 2 == 0)
+                        {
+                            StringBuilder sb = new StringBuilder();
+
+                            for (int i = lineArr.Length - 1; i >= 0; i--)
+                            {
+                                sb.Append(lineArr[i] + " ");
+                            }
+
+                            sb.Replace("-", "@");
+                            sb.Replace(",", "@");
+                            sb.Replace(".", "@");
+                            sb.Replace("!", "@");
+                            sb.Replace("?", "@");
+
+                            writer.WriteLine(sb.ToString());
+
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#Advanced/StreamsFilesDirectories/Exercise/P02.LineNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace P02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+                
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        int numberOfDigits = 0;
+                        int numbersOfLetters = 0;
+
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            if (!Char.IsLetterOrDigit(line[i]) && !Char.IsWhiteSpace(line[i]))
+                            {
+                                numberOfDigits++;
+                            }
+                            if (Char.IsLetter(line[i]))
+                            {
+                                numbersOfLetters++;
+                            }
+                        }
+                        
+                        writer.WriteLine($"Line {counter}: {line} ({numbersOfLetters})({numberOfDigits})");
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
