d8f713b6070de7bb95c14aaf0dc015c0ba2f4571
calevra1807@abv.bg <calevra1807@abv.bg>
2021-01-25 19:49:39 +0200

    StreamFilesDirectoriesProblem

---
+++ C#Advanced/StreamsFilesDirectories/Lab/P02.LineNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace P02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+
+                using(StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#Advanced/StreamsFilesDirectories/Lab/P04.MergeFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace P04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader readerOne = new StreamReader("../../../FileOne.txt"))
+            {
+                string lineOne = readerOne.ReadLine();
+
+                using (StreamReader readerTwo = new StreamReader("../../../FileTwo.txt"))
+                {
+                    string lineTwo = readerTwo.ReadLine();
+
+                    using (StreamWriter writer = new StreamWriter("../../../Output"))
+                    {
+                        while (lineOne != null)
+                        {
+                            writer.WriteLine(lineOne);
+                            writer.WriteLine(lineTwo);
+
+                            lineOne = readerOne.ReadLine();
+                            lineTwo = readerTwo.ReadLine();
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
