ba73bb9dc22211c9d289bce03b682c5b55b79831
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-01 10:21:55 +0200

    StackProblems

--- C#Advanced/StacksAndQueues/StacksAndQueueLab/P03.SimpleCalculator/StartUp.cs
+++ C#Advanced/StacksAndQueues/StacksAndQueueLab/P03.SimpleCalculator/StartUp.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace P03.SimpleCalculator
 {
@@ -8,36 +8,43 @@ namespace P03.SimpleCalculator
     {
         static void Main(string[] args)
         {
-            string[] expression = Console.ReadLine().Split(' ').Reverse().ToArray();
+            string[] expression = Console.ReadLine().Split(' ')
+                .Reverse().ToArray();
 
             Stack<string> stack = new Stack<string>(expression);
 
-            while(stack.Count > 1)
+            while (stack.Count > 1)
             {
-               // PrintStack(stack);
+                // PrintStack(stack);
 
                 int first = int.Parse(stack.Pop());
-
                 string sign = stack.Pop();
-
                 int second = int.Parse(stack.Pop());
 
-                switch(sign)
-                {
-                    case "+": 
-                        stack.Push((first + second).ToString());
-                        break;
-                    case "-":
-                        stack.Push((first - second).ToString());
-                        break;
-                    default:
-                        break;
-                }
-
+                CalculateStackStepByStep(stack, first, sign, second);
             }
 
             Console.WriteLine(stack.Pop());
         }
+
+        private static void CalculateStackStepByStep(Stack<string> stack, int first, string sign, int second)
+        {
+            switch (sign)
+            {
+                case "+":
+                    stack.Push((first + second).ToString());
+                    break;
+                case "-":
+                    stack.Push((first - second).ToString());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
         /*
         static void PrintStack(Stack<string> stack)
         {
--- C#Advanced/StacksAndQueues/StacksAndQueueLab/P04.MatchingBracjets/StartUp.cs
+++ C#Advanced/StacksAndQueues/StacksAndQueueLab/P04.MatchingBracjets/StartUp.cs
@@ -11,26 +11,21 @@ namespace P04.MatchingBrackets
 
             Stack<int> stack = new Stack<int>();
 
-            for(int i = 0; i <expression.Length; i++)
+            for (int i = 0; i < expression.Length; i++)
             {
-
-                if(expression[i] == '(')
+                if (expression[i] == '(')
                 {
-
                     stack.Push(i);
-
                 }
-                if(expression[i] == ')')
-                {
 
+                if (expression[i] == ')')
+                {
                     int start = stack.Pop();
 
                     Console.WriteLine(expression.Substring(start, i - start + 1));
-
                 }
 
             }
-
         }
     }
 }
