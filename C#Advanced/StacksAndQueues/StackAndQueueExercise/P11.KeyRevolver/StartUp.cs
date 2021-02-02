47440410499abd77f480953f2da1408bd5a40d1c
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-02 12:57:01 +0200

    StackAndQueueProblems

---
+++ C#Advanced/StacksAndQueues/StackAndQueueExercise/P11.KeyRevolver/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace P11.KeyRevolver
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int shoot = 0;
+            int bulletCount = 0;
+            bool isOpen = false;
+            StringBuilder sb = new StringBuilder();
+
+            double priceOfBullet = double.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+
+            int[] bullets = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stackBullets = new Stack<int>(bullets);
+
+            int[] locks = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queueLocks = new Queue<int>(locks);
+
+            double valueOfInteligence = double.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (!queueLocks.Any())
+                {
+                    isOpen = true;
+                    break;
+                }
+                if (!stackBullets.Any())
+                {
+                    break;
+                }
+
+                int currentBullet = stackBullets.Pop();
+                int currentLock = queueLocks.Peek();
+
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    queueLocks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                bulletCount++;
+                shoot++;
+                if (shoot.Equals(gunBarrelSize) && stackBullets.Any())
+                {
+                    Console.WriteLine("Reloading!");
+                    shoot = 0;
+                }
+            }
+
+            if (isOpen)
+            {
+                double bulletCoast = bulletCount * priceOfBullet;
+                valueOfInteligence -= bulletCoast;
+                sb.Append($"{stackBullets.Count} bullets left. Earned ${valueOfInteligence}");
+            }
+            else
+            {
+                sb.Append($"Couldn't get through. Locks left: {queueLocks.Count}");
+            }
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
---
+++ C#Advanced/StacksAndQueues/StackAndQueueExercise/P12.CupsAndBottles/StartUp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace P12.CupsAndBottles
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int wastetWater = 0;
+            bool noCups = false;
+            StringBuilder sb = new StringBuilder();
+
+            int[] cups = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queueCups = new Queue<int>(cups);
+
+            int[] bottles = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stakcBottles = new Stack<int>(bottles);
+
+            while (true)
+            {
+                int currentCup = queueCups.Peek();
+                int currentBottle = stakcBottles.Pop();
+
+                if (currentBottle >= currentCup)
+                {
+                    queueCups.Dequeue();
+                    wastetWater += (currentBottle - currentCup);
+                }
+                else
+                {
+                    currentCup -= currentBottle;
+                    while (currentCup > 0)
+                    {
+                        currentBottle = stakcBottles.Pop();
+                        if (currentCup > currentBottle)
+                        {
+                            currentCup -= currentBottle;
+                        }
+                        else
+                        {
+                            queueCups.Dequeue();
+                            wastetWater += (currentBottle - currentCup);
+                            currentCup -= currentBottle;
+                        }
+                    }
+                }
+
+                if (!queueCups.Any())
+                {
+                    noCups = true;
+                    break;
+                }
+                if (!stakcBottles.Any())
+                {
+                    break;
+                }
+            }
+
+            if (noCups)
+            {
+                sb.AppendLine($"Bottles: {String.Join(' ', stakcBottles)}")
+                  .AppendLine($"Wasted litters of water: {wastetWater}");
+            }
+            else
+            {
+                sb.AppendLine($"Cups: {String.Join(' ', queueCups)}")
+                  .AppendLine($"Wasted litters of water: {wastetWater}");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
