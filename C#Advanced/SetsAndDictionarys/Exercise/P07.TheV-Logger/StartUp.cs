4106d97c882b14e817f42c43734dab60ddaa069c
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-06 13:52:11 +0200

    SetAndDictionariesProblems

---
+++ C#Advanced/SetsAndDictionarys/Exercise/P07.TheV-Logger/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TheV_Logger
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                string[] inputInfo = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string vlogger = inputInfo[0];
+                string command = inputInfo[1];
+
+                if (command == "joined")
+                {
+                    if (!dict.ContainsKey(vlogger))
+                    {
+                        dict[vlogger] = new Dictionary<string, HashSet<string>>();
+
+                        dict[vlogger]["followers"] = new HashSet<string>();
+                        dict[vlogger]["following"] = new HashSet<string>();
+
+
+                        //  dict.Add(vlogger, new Dictionary<string, HashSet<string>>());
+                        //  dict[vlogger].Add("followers", new HashSet<string>());
+                        //  dict[vlogger].Add("following", new HashSet<string>());
+                    }
+                }
+
+                else if (command == "followed")
+                {
+                    string secondVlogger = inputInfo[2];
+
+                    if (dict.ContainsKey(vlogger) && dict.ContainsKey(secondVlogger) && vlogger != secondVlogger)
+                    {
+                        dict[vlogger]["following"].Add(secondVlogger);
+                        dict[secondVlogger]["followers"].Add(vlogger);
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {dict.Count} vloggers in its logs.");
+
+            dict = dict
+                .OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count)
+                .ToDictionary(a => a.Key, b => b.Value);
+
+            int number = 1;
+
+            foreach (var vlogger in dict)
+            {
+                Console.WriteLine($"{number}. {vlogger.Key} : {vlogger.Value["followers"].Count} followers, {vlogger.Value["following"].Count} following");
+
+                if (number == 1)
+                {
+                    foreach (string follower in vlogger.Value["followers"].OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                number++;
+            }
+
+        }
+    }
+}
---
+++ C#Advanced/SetsAndDictionarys/Exercise/P08.Ranking/StartUp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Ranking
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contestPassword = new Dictionary<string, string>();
+
+            Dictionary<string, Dictionary<string, int>> usersDict = new Dictionary<string, Dictionary<string, int>>();
+            string input;
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] inputArgs = input
+                    .Split(":")
+                    .ToArray();
+     
+                string contest = inputArgs[0];
+                string password = inputArgs[1];
+
+                contestPassword[contest] = password;
+            }
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+
+                string[] inputArgs = input .Split("=>").ToArray();
+                
+                string contest = inputArgs[0];
+                string password = inputArgs[1];
+                string username = inputArgs[2];
+                int points = int.Parse(inputArgs[3]);
+
+                if (contestPassword.ContainsKey(contest) && contestPassword[contest] == password)
+                {
+                    if (!usersDict.ContainsKey(username))
+                    {
+                        usersDict[username] = new Dictionary<string, int>();
+                        usersDict[username][contest] = points;
+
+                    }
+
+                    else if (!usersDict[username].ContainsKey(contest))
+                    {
+                        usersDict[username][contest] = points;
+                    }
+
+                    else if (usersDict[username].ContainsKey(contest))
+                    {
+                        if (usersDict[username][contest] < points)
+                        {
+                            usersDict[username][contest] = points;
+                        }
+                    }
+                }
+
+            }
+
+            Dictionary<string, int> usernameAndPoints = new Dictionary<string, int>();
+
+            foreach (var user in usersDict)
+            {
+                usernameAndPoints[user.Key] = user.Value.Values.Sum();
+            }
+
+            foreach (var username in usernameAndPoints.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"Best candidate is {username.Key} with total {username.Value} points.");
+                break;
+            }
+
+            Console.WriteLine("Ranking: ");
+
+            foreach (var username in usersDict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(username.Key);
+
+                foreach (var contest in username.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+
+        }
+    }
+}
