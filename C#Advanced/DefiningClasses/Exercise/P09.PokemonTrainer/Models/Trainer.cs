28a374c72ead642d7ccf6e67e9a03f6e1a991caf
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-09 17:36:40 +0200

    ClassProblem

---
+++ C#Advanced/DefiningClasses/Exercise/P09.PokemonTrainer/Models/Trainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P09.PokemonTrainer.Models
+{
+    public class Trainer
+    {
+        private string name;
+        private int bagesCount;
+        private Stack<Pokemon> pokemons;
+
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.BagesCount = 0;
+            this.Pokemons = new Stack<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int BagesCount { get; set; }
+
+        public Stack<Pokemon> Pokemons { get; set; }
+
+        public void AddBages()
+        {
+            this.BagesCount++;
+        }
+
+        public void RemovePokemon()
+        {
+            if(this.Pokemons.Count > 0 && this.Pokemons.Where(p => p.Health <= 0).FirstOrDefault() != null)
+            {
+                this.Pokemons = new Stack<Pokemon>(this.Pokemons.Where(p => p.Health > 0));
+            }
+        }
+
+       
+    }
+}
---
+++ C#Advanced/DefiningClasses/Exercise/P09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using P09.PokemonTrainer.Models;
+
+namespace P09.PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Queue<Trainer> trainers = new Queue<Trainer>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputArgs = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                string trainerName = inputArgs[0];
+                string pokemonName = inputArgs[1];
+                string element = inputArgs[2];
+                int health = int.Parse(inputArgs[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, health);
+
+                var currentTrainer = trainers.Where(t => t.Name == trainerName).FirstOrDefault();
+
+                if(currentTrainer == null)
+                {
+                    currentTrainer = new Trainer(trainerName);
+                    currentTrainer.Pokemons.Push(pokemon);
+                    trainers.Enqueue(currentTrainer);
+                }
+
+                else
+                {
+                    currentTrainer.Pokemons.Push(pokemon);
+                }
+               
+            }
+
+            string command;
+            while((command = Console.ReadLine().Trim()) != "End")
+            {
+                foreach(var trainer in trainers)
+                {
+                    if(trainer.Pokemons.Where(p => p.Element == command).FirstOrDefault() == null)
+                    {
+                        foreach(var pokemon in trainer.Pokemons)
+                        {
+                            pokemon.ReduceHealth();
+                        }
+
+                        trainer.RemovePokemon();
+                    }
+
+                    else
+                    {
+                        trainer.AddBages();
+                    }
+
+                }
+
+            }
+
+
+            Console.WriteLine(String.Join(Environment.NewLine, trainers
+                .OrderByDescending(t => t.BagesCount)
+                .Select(t => $"{t.Name} {t.BagesCount} {t.Pokemons.Count}")));
+
+        }
+    }
+}
