f900cdf13a16021371bb9e976791370513d6c678
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-12 10:14:53 +0200

    ExamProblems

---
+++ C#Advanced/ExamPractice/P01.Cooking/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01.Cooking
+{
+    class Program
+    {
+        const int BREAD = 25;
+        const int CAKE = 50;
+        const int PASTRY = 75;
+        const int FRUIT_PIE = 100;
+        const int INCREASE = 3;
+
+        static void Main(string[] args)
+        {
+            int[] liquidInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>(liquidInput);
+
+            int[] ingredientInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(ingredientInput);
+
+            int breadCount = 0;
+            int cakeCount = 0;
+            int pastryCount = 0;
+            int fruitPieCount = 0;
+            bool isColected = false;
+           
+            while (true)
+            {
+                if(!queue.Any() || !stack.Any())
+                {
+                    break;
+                }
+
+                int currLiquid = queue.Dequeue();
+                int currIngredient = stack.Pop();
+
+                int sum = currLiquid + currIngredient;
+
+                if(sum == BREAD)
+                {
+                    breadCount++;
+                }
+                else if(sum == CAKE)
+                {
+                    cakeCount++;
+                }
+                else if(sum == PASTRY)
+                {
+                    pastryCount++;
+                }
+                else if(sum == FRUIT_PIE)
+                {
+                    fruitPieCount++;
+                }
+                else
+                {
+                    currIngredient += INCREASE;
+                    stack.Push(currIngredient);
+                }
+
+                if(IsDone(breadCount, cakeCount, pastryCount, fruitPieCount))
+                {
+                    isColected = true;
+                }
+
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (isColected)
+            {
+                sb.AppendLine($"Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                sb.AppendLine($"Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            string queueStr = queue.Any() ? String.Join(", ", queue) : "none";
+            sb.AppendLine($"Liquids left: {queueStr}");
+            string stackStr = stack.Any() ? String.Join(", ", stack) : "none";
+            sb.AppendLine($"Ingredients left: {stackStr}");
+
+            sb.AppendLine($"Bread: {breadCount}");
+            sb.AppendLine($"Cake: {cakeCount}");
+            sb.AppendLine($"Fruit Pie: {fruitPieCount}");
+            sb.AppendLine($"Pastry: {pastryCount}");
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+
+        }
+
+
+
+
+        private static bool IsDone(int bread, int cake, int pastry, int fruitPie)
+        {
+            if(bread > 0 && cake > 0 && pastry > 0 && fruitPie > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#Advanced/ExamPractice/P02.Seeling/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+
+namespace P02.Seeling
+{
+    class Program
+    {
+        public class Position
+        {
+            public Position(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+
+            public int Row { get; set; }
+
+            public int Col { get; set; }
+
+            public bool IsSafe(int rowCount, int colCount)
+            {
+                if(Row < 0 || Col < 0)
+                {
+                    return false;
+                }
+                if(Row >= rowCount || Col >= colCount)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            Read(matrix);
+            var player = GetPlayerPosition(matrix);
+
+            int firstPortalRow = -1;
+            int firstPortalCol = -1;
+
+            int secondPortalRow = -1;
+            int secondPortalCol = -1;
+
+            int count = 0;
+            int money = 0;
+
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(matrix[row, col] == 'O' && count == 0)
+                    {
+                        firstPortalRow = row;
+                        firstPortalCol = col;
+                        count++;
+                    }
+
+                    if(matrix[row, col] == 'O' && count == 1)
+                    {
+                        secondPortalRow = row;
+                        secondPortalCol = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                matrix[player.Row, player.Col] = '-';
+
+                if(command == "left")
+                {
+                    player.Col--;
+                    if(!player.IsSafe(n, n))
+                    {
+                        Console.WriteLine($"Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        Print(matrix);
+                        return;
+                    }
+                }
+                if (command == "right")
+                {
+                    player.Col++;
+                    if (!player.IsSafe(n, n))
+                    {
+                        Console.WriteLine($"Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        Print(matrix);
+                        return;
+                    }
+                }
+                if (command == "Up")
+                {
+                    player.Row--;
+                    if (!player.IsSafe(n, n))
+                    {
+                        Console.WriteLine($"Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        Print(matrix);
+                        return;
+                    }
+                }
+                if (command == "down")
+                {
+                    player.Row++;
+                    if (!player.IsSafe(n, n))
+                    {
+                        Console.WriteLine($"Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        Print(matrix);
+                        return;
+                    }
+                }
+
+
+                if(matrix[player.Row, player.Col] == 'O')
+                {
+                    matrix[player.Row, player.Col] = '-';
+                    if (matrix[player.Row, player.Col] == matrix[firstPortalRow, firstPortalCol])
+                    {
+                        player.Row = secondPortalRow;
+                        player.Col = secondPortalCol;
+                    }
+                    else if(matrix[player.Row, player.Col] == matrix[secondPortalRow, secondPortalCol])
+                    {
+                        player.Row = firstPortalRow;
+                        player.Col = firstPortalCol;
+                    }
+                }
+
+                if(Char.IsDigit(matrix[player.Row, player.Col]))
+                {
+                  string number = matrix[player.Row, player.Col].ToString();
+                    money += int.Parse(number);
+
+                }
+
+                matrix[player.Row, player.Col] = 'S';
+                if(money >= 50)
+                {
+                    Console.WriteLine($"Good news! You succeeded in collecting enough money!");
+                    Console.WriteLine($"Money: {money}");
+                    Print(matrix);
+                    return;
+                }
+            }
+
+
+        }
+
+        static Position GetPlayerPosition(char[,] matrix)
+        {
+            Position position = null;
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(matrix[row, col] == 'S')
+                    {
+                        position = new Position(row, col);
+                    }
+                }
+            }
+
+            return position;
+        }
+
+        private static void Read(char[,] matrix)
+        {
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string line = Console.ReadLine();
+                for(int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+        }
+
+        private static void Print(char[,] matrix)
+        {
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
