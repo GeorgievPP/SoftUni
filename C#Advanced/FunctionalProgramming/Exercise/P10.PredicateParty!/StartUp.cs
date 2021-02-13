21fed553a2aee86fc192b0a93a046f1b9182b351
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-13 12:47:07 +0200

    FunctionalProblems*

--- C#Advanced/FunctionalProgramming/Exercise/P10.PredicateParty!/StartUp.cs
+++ C#Advanced/FunctionalProgramming/Exercise/P10.PredicateParty!/StartUp.cs
@@ -8,115 +8,80 @@ namespace P10.PredicateParty_
     {
         static void Main(string[] args)
         {
-
             List<string> guest = Console.ReadLine().Split().ToList();
 
             string command;
-
             while ((command = Console.ReadLine()) != "Party!")
             {
-
                 string[] cmdArgs = command.Split(' ').ToArray();
-
                 string cmdType = cmdArgs[0];
-
                 string[] predicateArgs = cmdArgs.Skip(1).ToArray();
 
                 Predicate<string> predicate = GetPredicate(predicateArgs);
 
                 if (cmdType == "Remove")
                 {
-
                     guest.RemoveAll(predicate);
-
                 }
 
                 else if (cmdType == "Double")
                 {
-
                     for (int i = 0; i < guest.Count; i++)
                     {
-
                         string currentGuest = guest[i];
 
                         if (predicate(currentGuest))
                         {
-
                             guest.Insert(i + 1, currentGuest);
-
                             i++;
-
                         }
-
                     }
-
                 }
-
             }
 
             if (guest.Count == 0)
             {
-
                 Console.WriteLine("Nobody is going to the party!");
-
             }
 
             else
             {
-
                 Console.WriteLine($"{String.Join(", ", guest)} are going to the party!");
-
             }
 
         }
 
         static Predicate<string> GetPredicate(string[] predicateArgs)
         {
-
             Predicate<string> predicate = null;
-
             string prType = predicateArgs[0];
-
             string prArg = predicateArgs[1];
 
             if (prType == "StartsWith")
             {
-
                 predicate = new Predicate<string>((name) =>
                 {
-
                     return name.StartsWith(prArg);
-
                 });
-
             }
 
             else if (prType == "EndsWith")
             {
-
                 predicate = new Predicate<string>((name) =>
                 {
-
                     return name.EndsWith(prArg);
-
                 });
-
             }
 
             else if (prType == "Length")
             {
-
                 predicate = new Predicate<string>((name) =>
                 {
-
                     return name.Length == int.Parse(prArg);
-
                 });
-
             }
 
             return predicate;
-
         }
     }
 }
---
+++ C#Advanced/FunctionalProgramming/Exercise/P11.PartyReservationFilterModule/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11.ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = new List<string>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            List<string> filters = new List<string>();
+
+            string input;
+            while((input = Console.ReadLine()) != "Print")
+            {
+                var commands = input.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                if (commands[0] == "Add filter")
+                {
+                    filters.Add(commands[1] + " " + commands[2]);
+                }
+                else if (commands[0] == "Remove filter")
+                {
+                    filters.Remove(commands[1] + " " + commands[2]);
+                }
+
+            }
+
+            foreach( var filter in filters)
+            {
+                var commands = filter.Split();
+                if (commands[0] == "Starts")
+                {
+                    guests = guests.Where(p => !p.StartsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Ends")
+                {
+                    guests = guests.Where(p => !p.EndsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Length")
+                {
+                    guests = guests.Where(p => p.Length != int.Parse(commands[1])).ToList();
+                }
+                else if (commands[0] == "Contains")
+                {
+                    guests = guests.Where(p => !p.Contains(commands[1])).ToList();
+                }
+
+            }
+
+            if (guests.Any())
+            {
+                Console.WriteLine(string.Join(" ", guests));
+            }
+        }
+    }
+}
