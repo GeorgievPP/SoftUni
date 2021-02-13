5bcfc8dea5110a18863d6276f9276c50cdf31b2c
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-13 17:45:29 +0200

    examProblemsss

---
+++ C#Advanced/ExamPractice/P03.Guild/Guild.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if(this.roster.Count < this.Capacity)
+            {
+                this.roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = this.roster.Where(p => p.Name == name).FirstOrDefault();
+            if(player != null)
+            {
+                this.roster.Remove(player);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            if(this.roster.Any(p => p.Name == name && p.Rank == "Trial"))
+            {
+                foreach (var player in roster.Where(n => n.Name == name))
+                {
+                    player.Rank = "Member";
+                    break;
+                }
+            }
+            
+        }
+
+        public void DemotePlayer(string name)
+        {
+            if (this.roster.Any(p => p.Name == name && p.Rank == "Member"))
+            {
+                foreach (var player in roster.Where(n => n.Name == name))
+                {
+                    player.Rank = "Trial";
+                    break;
+                }
+            }
+
+        }
+
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] kickedClass = this.roster.Where(p => p.Class == @class).ToArray();
+            this.roster = this.roster.Where(p => p.Class != @class).ToList();
+            if (kickedClass.Any())
+            {
+                return kickedClass;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            if (this.roster.Any())
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Players in the guild: {this.Name}");
+                foreach (var player in roster)
+                {
+                    sb.AppendLine(player.ToString());
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ C#Advanced/ExamPractice/P03.VetClinic/Clinic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void Add(Pet pet)
+        {
+            if(this.data.Count < this.Capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = this.data.Where(n => n.Name == name).FirstOrDefault();
+            if(pet != null)
+            {
+                this.data.Remove(pet);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = this.data.Where(p => p.Name == name && p.Owner == owner).FirstOrDefault();
+
+            if(pet != null)
+            {
+                return pet;
+            }
+
+            return null;
+        }
+
+
+        public Pet GetOldestPet()
+        {
+            if (this.data.Any())
+            {
+                Pet pet = this.data.OrderByDescending(p => p.Age).First();
+                return pet;
+            }
+
+            return null;
+        }
+
+        public string GetStatistics()
+        {
+            if (this.data.Any())
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"The clinic has the following patients:");
+                foreach (var pet in data)
+                {
+                    sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+
+            return null;
+        }
+    }
+}
