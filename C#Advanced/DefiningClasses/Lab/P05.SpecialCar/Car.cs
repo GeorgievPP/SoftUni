2a970e69c81f5253c338c3711c5ff44855415aab
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-15 08:34:20 +0200

    classProblem

---
+++ C#Advanced/DefiningClasses/Lab/P05.SpecialCar/Car.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace P05.SpecialCars
+{
+    class Car
+    {
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        {
+
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.Engine = engine;
+            this.Tires = tires;
+
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+
+
+        public void Drive(double distance)
+        {
+
+            double rideExpenses = this.FuelConsumption * distance / 100;
+
+            if (rideExpenses > this.FuelQuantity)
+            {
+
+                Console.WriteLine("Not enough fuel to perform this trip!");
+
+            }
+            else
+            {
+
+                this.FuelQuantity -= distance / 100 * this.FuelConsumption;
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"HorsePowers: {this.Engine.HorsePower}");
+            sb.AppendLine($"FuelQuantity: {this.FuelQuantity}");
+
+            return sb.ToString();
+
+        }
+    }
+
+
+}
---
+++ C#Advanced/DefiningClasses/Lab/P05.SpecialCar/Engine.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace P05.SpecialCars
+{
+    public class Engine
+    {
+
+        private int horsePower;
+        private double cubicCapacity;
+
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+
+        }
+
+        public double CubicCapacity
+        {
+
+            get
+            {
+                return this.cubicCapacity;
+            }
+            set
+            {
+                this.cubicCapacity = value;
+            }
+
+        }
+    }
+}
