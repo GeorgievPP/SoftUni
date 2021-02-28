54970f81a6cc7a702b801a64b7575cee4610f76e
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-28 10:37:02 +0200

    InterfacesAndAbstraction

---
+++ C#OOP/InterfacesAndAbstraction/Lab/P02.Cars/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.Models
+{
+    public class Car : ICar
+    {
+        public Car(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            string start = $"Engine start";
+            return start;
+        }
+
+        public string Stop()
+        {
+            string stop = $"Breaaak!";
+            return stop;
+        }
+
+       
+    }
+}
---
+++ C#OOP/InterfacesAndAbstraction/Lab/P02.Cars/Models/Cars/Tesla.cs
@@ -0,0 +1,29 @@
+﻿using Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.Models.Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery) : base(model, color)
+        {
+            this.Battery = battery;
+        }
+
+        public int Battery { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} {nameof(Tesla)} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
