22a96392b522b4e395f83d84bf3deac119d5b536
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-28 18:15:35 +0200

    Polymorphism

---
+++ C#OOP/Polymorphism/Exercise/P01.Vehicles/Models/Vehicles/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Vehicles.Models.Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double ADDITIONAL_CONSUMPTION_PER_KM = 1.6;
+        private const double REFUELING_COEFFICIENT = 0.95;
+
+        public Truck(double fuelQuantity, double fuelConsimption) 
+            : base(fuelQuantity, fuelConsimption)
+        {
+        }
+
+        protected override double AdditionalConsumption => ADDITIONAL_CONSUMPTION_PER_KM;
+
+        public override void Refuel(double fuel)
+        {
+            base.Refuel(fuel * REFUELING_COEFFICIENT);
+        }
+    }
+}
---
+++ C#OOP/Polymorphism/Exercise/P01.Vehicles/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using P01.Vehicles.Models;
+using P01.Vehicles.Models.Vehicles;
+using System;
+
+namespace P01.Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Vehicle car = new Car(double.Parse(carInput[1]), double.Parse(carInput[2]));
+            string[] truckInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Vehicle truck = new Truck(double.Parse(truckInput[1]), double.Parse(truckInput[2]));
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string type = cmdArgs[1];
+                string command = cmdArgs[0];
+                double value = double.Parse(cmdArgs[2]);
+
+                switch (type)
+                {
+                    case nameof(Car):
+                        ExecuteCommand(car, command, value);
+                        break;
+                    case nameof(Truck):
+                        ExecuteCommand(truck, command, value);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+
+        }
+
+        private static void ExecuteCommand(Vehicle vehicle, string command, double value)
+        {
+            switch (command)
+            {
+                case "Drive":
+                    Console.WriteLine(vehicle.Drive(value));
+                    break;
+                case "Refuel":
+                    vehicle.Refuel(value);
+                    break;
+            }
+        }
+    }
+}
