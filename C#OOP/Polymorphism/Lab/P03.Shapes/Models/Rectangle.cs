70d35e7ab837e88667161a1be433e1674c13ad43
calevra1807@abv.bg <calevra1807@abv.bg>
2021-02-28 12:56:45 +0200

    PolymorphismProblems

---
+++ C#OOP/Polymorphism/Lab/P03.Shapes/Models/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.Width + 2 * this.Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
