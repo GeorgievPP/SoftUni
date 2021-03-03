c9e464cd7e33933996d727c0c9f0178a86fbc375
calevra1807@abv.bg <calevra1807@abv.bg>
2021-03-03 11:23:49 +0200

    ReflectionProblems

---
+++ C#OOP/ReflectionAndAttributes/Lab/P01.Stealer/Models/Hacker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stealer.Models
+{
+    public class Hacker
+    {
+        public string username = "securityGod82";
+        private string password = "mySuperSecretPassw0rd";
+
+        public string Password
+        {
+            get => this.password;
+            set => this.password = value;
+        }
+
+        public int Id { get; set; }
+
+        public double BankAccountBAlance { get; private set; }
+
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+
+        }
+    }
+}
---
+++ C#OOP/ReflectionAndAttributes/Lab/P01.Stealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Stealer.Models;
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Models.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
