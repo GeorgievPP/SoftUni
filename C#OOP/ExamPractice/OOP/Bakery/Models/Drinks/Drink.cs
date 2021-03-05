55e8ae633695f053acb30ba5470d0ea1a7f6f4c2
calevra1807@abv.bg <calevra1807@abv.bg>
2021-03-05 23:30:22 +0200

    ExamProblem

---
+++ C#OOP/ExamPractice/OOP/Bakery/Models/Drinks/Drink.cs
@@ -0,0 +1,18 @@
+﻿using Bakery.Models.Drinks.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Drinks
+{
+    public class Drink : IDrink
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Portion => throw new NotImplementedException();
+
+        public decimal Price => throw new NotImplementedException();
+
+        public string Brand => throw new NotImplementedException();
+    }
+}
