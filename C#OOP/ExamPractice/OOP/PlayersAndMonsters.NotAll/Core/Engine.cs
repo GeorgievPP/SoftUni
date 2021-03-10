0c6f46b31a650c2e1f1ac4772b0e53536ea998fc
calevra1807@abv.bg <calevra1807@abv.bg>
2021-03-10 23:09:21 +0200

    examProblem

---
+++ C#OOP/ExamPractice/OOP/PlayersAndMonsters.NotAll/Core/Engine.cs
@@ -0,0 +1,74 @@
+﻿using PlayersAndMonsters.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Core
+{
+    public class Engine : IEngine
+    {
+        private IManagerController controller;
+
+        public Engine()
+        {
+            this.controller = new ManagerController();
+        }
+
+        public void Run()
+        {
+            string input;
+            while((input = Console.ReadLine()) != "Exit")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string command = cmdArgs[0];
+
+                string result = string.Empty;
+
+                try
+                {
+                    switch (command)
+                    {
+                        case "AddPlayer":
+                            string type = cmdArgs[1];
+                            string username = cmdArgs[2];
+
+                            result = controller.AddPlayer(type, username);
+                            break;
+
+                        case "AddCard":
+                            string cardType = cmdArgs[1];
+                            string name = cmdArgs[2];
+
+                            result = controller.AddCard(cardType, name);
+                            break;
+
+                        case "AddPlayerCard":
+                            string usernamePlayer = cmdArgs[1];
+                            string cardName = cmdArgs[2];
+
+                            result = this.controller.AddPlayerCard(usernamePlayer, cardName);
+                            break;
+
+                        case "Fight":
+                            string attacker = cmdArgs[1];
+                            string enemy = cmdArgs[2];
+
+                            result = this.controller.Fight(attacker, enemy);
+                            break;
+
+                        case "Report":
+                            result = this.controller.Report();
+                            break;
+                    }
+
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
---
+++ C#OOP/ExamPractice/OOP/PlayersAndMonsters.NotAll/Repositories/PlayerRepository.cs
@@ -0,0 +1,61 @@
+﻿using PlayersAndMonsters.Models.Players;
+using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private int count;
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public int Count => this.players.Count;
+
+        public IReadOnlyCollection<IPlayer> Players => this.players;
+
+        public object Any => throw new NotImplementedException();
+
+        public void Add(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            if (this.players.Any())
+            {
+                IPlayer player2 = this.players.FirstOrDefault(x => x.Username == player.Username);
+                if (player2 != null)
+                {
+                    throw new ArgumentException($"Player {player.Username} already exists!");
+                }
+            }
+
+            this.players.Add(player);
+        }
+
+        public IPlayer Find(string username)
+        {
+            return this.players.FirstOrDefault(x => x.Username == username);
+        }
+
+        public bool Remove(IPlayer player)
+        {
+            if(player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+
+            return this.players.Remove(player);
+        }
+    }
+}
