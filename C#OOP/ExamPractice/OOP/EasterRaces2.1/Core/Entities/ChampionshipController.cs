606661e547934c3008ad30f209b597711ccf634d
calevra1807@abv.bg <calevra1807@abv.bg>
2021-03-16 12:42:05 +0200

    practiceExamProblem

---
+++ C#OOP/ExamPractice/OOP/EasterRaces2.1/Core/Entities/ChampionshipController.cs
@@ -0,0 +1,145 @@
+﻿using EasterRaces.Core.Contracts;
+using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Cars.Entities;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Models.Drivers.Entities;
+using EasterRaces.Models.Races.Entities;
+using EasterRaces.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Core.Entities
+{
+    public class ChampionshipController : IChampionshipController
+    {
+        //Maybe
+        private CarRepository cars;
+        private DriverRepository drivers;
+        private RaceRepository races;
+
+        public ChampionshipController()
+        {
+            this.cars = new CarRepository();
+            this.drivers = new DriverRepository();
+            this.races = new RaceRepository();
+        }
+
+
+        public string AddCarToDriver(string driverName, string carModel)
+        {
+            var driver = this.drivers.GetByName(driverName);
+            if(driver == null)
+            {
+                throw new InvalidOperationException($"Driver {driverName} could not be found.");
+            }
+
+            var car = this.cars.GetByName(carModel);
+            if(car == null)
+            {
+                throw new InvalidOperationException($"Car {carModel} could not be found.");
+            }
+
+            driver.AddCar(car);
+
+            return $"Driver {driverName} received car {carModel}.";
+        }
+
+        public string AddDriverToRace(string raceName, string driverName)
+        {
+            var race = this.races.GetByName(raceName);
+            if(race == null)
+            {
+                throw new InvalidOperationException($"Race {raceName} could not be found.");
+            }
+
+            var driver = this.drivers.GetByName(driverName);
+            if(driver == null)
+            {
+                throw new InvalidOperationException($"Driver {driverName} could not be found.");
+            }
+
+            race.AddDriver(driver);
+
+            return $"Driver {driverName} added in {raceName} race.";
+        }
+
+        public string CreateCar(string type, string model, int horsePower)
+        {
+            ICar car = null;
+            if(type == "Muscle")
+            {
+                car = new MuscleCar(model, horsePower);
+            }
+            else if(type == "Sports")
+            {
+                car = new SportsCar(model, horsePower);
+            }
+
+            var c1 = this.cars.GetByName(car.Model);
+            if(c1 != null)
+            {
+                throw new ArgumentException($"Car {model} is already created.");
+            }
+
+            this.cars.Add(car);
+
+            return $"{car.GetType().Name} {model} is created.";
+        }
+
+        public string CreateDriver(string driverName)
+        {
+            var driver = this.drivers.GetByName(driverName);
+            if(driver != null)
+            {
+                throw new ArgumentException($"Driver {driverName} is already created.");
+            }
+
+            driver = new Driver(driverName);
+            this.drivers.Add(driver);
+
+            return $"Driver {driverName} is created.";
+        }
+
+        public string CreateRace(string name, int laps)
+        {
+            var race = this.races.GetByName(name);
+            if(race != null)
+            {
+                throw new InvalidOperationException($"Race {name} is already create.");
+            }
+
+            race = new Race(name, laps);
+            this.races.Add(race);
+
+            return $"Race {name} is created.";
+        }
+
+        public string StartRace(string raceName)
+        {
+            var race = this.races.GetByName(raceName);
+            if(race == null)
+            {
+                throw new InvalidOperationException($"Race {raceName} could not be found.");
+            }
+
+            if(race.Drivers.Count < 3)
+            {
+                throw new InvalidOperationException($"Race {raceName} cannot start with less than 3 participants.");
+            }
+
+            var raceTop = race.Drivers.OrderByDescending(x => x.Car.CalculateRacePoints(race.Laps));
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Driver {raceTop.First().Name} wins {raceName} race.");
+            sb.AppendLine($"Driver {raceTop.Skip(1).First().Name} is second in {raceName} race.");
+            sb.AppendLine($"Driver {raceTop.Skip(2).First().Name} is third in {raceName} race.");
+
+            this.races.Remove(race);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
