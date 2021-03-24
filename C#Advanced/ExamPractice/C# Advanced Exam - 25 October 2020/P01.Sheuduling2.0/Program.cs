48d5b2d2250c4b3b202d37b1da3cf58b334f6096
calevra1807@abv.bg <calevra1807@abv.bg>
2021-03-24 17:47:43 +0200

    examProblems

---
+++ C#Advanced/ExamPractice/C# Advanced Exam - 25 October 2020/P01.Sheuduling2.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Sheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stackInput = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int[] queueInput = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse).ToArray();
+
+            int magicTask = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(stackInput);
+            Queue<int> queue = new Queue<int>(queueInput);
+
+            while (true)
+            {
+                int task = stack.Peek();
+                int thread = queue.Peek();
+
+                if(task == magicTask)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {task}");
+                    Console.WriteLine(String.Join(" ", queue));
+                    return;
+                }
+
+                if(thread >= task)
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                }
+                else if(thread < task)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+        }
+    }
+}
---
+++ C#Advanced/ExamPractice/C# Advanced Exam - 25 October 2020/P02.Garden2.0/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            Read(matrix);
+
+            string input;
+            while((input = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] coordinates = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+
+                if(coordinates[0] < 0 || coordinates[1] < 0 || coordinates[0] >= matrixSize[0] || coordinates[1] >= matrixSize[1])
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if(row == coordinates[0] || col == coordinates[1])
+                        {
+                            matrix[row, col] += 1;
+                        }
+                    }
+                }
+
+            }
+
+            Print(matrix);
+
+        }
+
+        private static void Read(int[,] matrix)
+        {
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+        }
+
+        private static void Print(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
