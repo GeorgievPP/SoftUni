11fe9e86065a25f44cb5d586e7e477568aa4916e
calevra1807@abv.bg <calevra1807@abv.bg>
2021-05-30 15:37:41 +0300

    newJsDomLab

--- JSAdvanced/May2021/04.DOM/Lab/02.Collect List Items/CollectListItems.js
+++ JSAdvanced/May2021/04.DOM/Lab/02.Collect List Items/CollectListItems.js
@@ -1,6 +1,11 @@
 function extractText() {
-    let listElements = document.querySelectorAll('#items li');
+    /*let listElements = document.querySelectorAll('#items li');
     for(let li of listElements){
         document.getElementById('result').value += li.textContent + "\n";
-    }
+    }*/
+
+    const liElements = [...document.getElementsByTagName('li')];
+    const elementText = liElements.map(e => e.textContent);
+
+    document.getElementById('result').value = elementText.join('\n');
 }
