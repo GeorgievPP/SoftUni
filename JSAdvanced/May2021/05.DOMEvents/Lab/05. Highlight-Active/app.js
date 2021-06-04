82261a986ba968603cf52fa2b94bcf19e32afef5
calevra1807@abv.bg <calevra1807@abv.bg>
2021-06-04 11:10:45 +0300

    domEvents

---
+++ JSAdvanced/May2021/05.DOMEvents/Lab/05. Highlight-Active/app.js
@@ -0,0 +1,16 @@
+function focused() {
+    Array.from(document.querySelectorAll('input')).forEach(i => {
+        i.addEventListener('focus', onFocus);
+        i.addEventListener('blur', onBlur);
+    });
+
+    function onFocus(ev) {
+        //onFocus -> select input parent -> apply class "focused"
+        ev.target.parentNode.className = 'focused';
+    }
+
+    function onBlur(ev) {
+        //onBlur -> select input parent -> remove class "focused"
+        ev.target.parentNode.className = '';
+    }
+}
