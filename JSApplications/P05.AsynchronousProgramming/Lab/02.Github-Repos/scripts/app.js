35fa8b861c96169f689fce9f0186128c3801d3a2
calevra1807@abv.bg <calevra1807@abv.bg>
2021-07-01 22:11:12 +0300

    JSApp

---
+++ JSApplications/P05.AsynchronousProgramming/Lab/02.Github-Repos/scripts/app.js
@@ -0,0 +1,38 @@
+function loadRepos() {
+	const username = document.getElementById('username').value;
+
+	const url = `http://api.github.com/users/${username}/repos`;
+
+	// 1
+/*
+	const requestPromise = fetch(url);
+
+	console.log(requestPromise);
+
+	requestPromise.then(handleResponse);
+
+	function handleResponse(response) {
+		console.log(response);
+		const dataPromise = response.json();
+		dataPromise.then(handleData);
+	}
+
+	function handleData(data) {
+		console.log(data);
+	}
+*/
+	//2 same
+	 
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			const ulElement = document.getElementById('repos');
+			ulElement.innerHTML = '';
+			data.forEach(r => {
+				const liElement = document.createElement('li');
+				liElement.textContent = r.full_name;
+				ulElement.appendChild(liElement);
+			});
+		});
+
+}
