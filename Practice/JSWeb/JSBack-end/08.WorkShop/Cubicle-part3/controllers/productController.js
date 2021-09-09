0d47a81df5766ff6838883c6437859782644af16
calevra1807@abv.bg <calevra1807@abv.bg>
2021-09-09 21:17:23 +0300

    jsBE08

---
+++ Practice/JSWeb/JSBack-end/08.WorkShop/Cubicle-part3/controllers/productController.js
@@ -0,0 +1,112 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const cubes = await req.storage.getAll(req.query);
+
+    const ctx = {
+        title: 'Cubicle',
+        cubes,
+        search: req.query.search || '',
+        from: req.query.from || '',
+        to: req.query.to || ''
+    };
+
+    res.render('index', ctx);
+});
+
+router.get('/create', (req, res) => {
+    res.render('create', { title: 'Create Cube' });
+});
+router.post('/create', async (req, res) => {
+    const cube = {
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        difficulty: Number(req.body.difficulty)
+    };
+
+    try {
+        await req.storage.create(cube);
+    } catch (err) {
+        if (err.name == 'ValidationError') {
+            return res.render('create', { title: 'Create Cube', error: 'All fields are required. Image URL must be a valid URL' })
+        }
+    }
+
+    res.redirect('/');
+});
+
+router.get('/details/:id',  async (req, res) => {
+    console.log(req.params.id)
+    const cube = await req.storage.getById(req.params.id);
+    if (cube == undefined) {
+        res.redirect('/404');
+    } else {
+        console.log(cube);
+        const ctx = {
+            title: 'Cubicle',
+            cube
+        };
+        res.render('details', ctx);    
+    }
+});
+
+router.get('/edit/:id', async (req, res) => {
+    console.log(req.params.id);
+
+    const cube = await req.storage.getById(req.params.id);
+    cube[`select${cube.difficulty}`] = true;
+    console.log(cube);
+
+    if (!cube) {
+        res.redirect('/404');
+    } else {
+        const ctx =  {
+            title: 'Edit Cube',
+            cube 
+        };
+        res.render('edit', ctx);
+    }
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const cube = {
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        difficulty: Number(req.body.difficulty)
+    };
+
+    try {
+        await req.storage.edit(req.params.id, cube);
+        res.redirect('/');
+    } catch(err) {
+        res.redirect('/404');
+    }
+});
+
+router.get('/attach/:cubeId',  async (req, res) => {
+    const cube = await req.storage.getById(req.params.cubeId);
+    const accessories = await req.storage.getAllAccessories((cube.accessories || []).map(a => a._id));
+
+    res.render('attach', {
+        title: 'Attach Stickers',
+        cube,
+        accessories
+    });
+});
+
+router.post('/attach/:cubeId', async (req, res) => {
+    console.log(req.body);
+    const cubeId = req.params.cubeId;
+    const stickerId = req.body.accessory;
+    console.log(stickerId);
+
+    await req.storage.attachSticker(cubeId, stickerId); // try-catch
+
+    res.redirect(`/details/${cubeId}`);
+});
+
+module.exports = router;
