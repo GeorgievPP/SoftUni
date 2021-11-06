054004b8695b4a7614f8e2367d8b50e0ea753da2
Petyo Georgiev <calevra1807@abv.bg>
2021-11-06 18:55:11 +0200

    EntityRelationExercise2

--- EntityFramework/EntityRelations/ErExercise/ErExercise/Data/Models/Student.cs
+++ EntityFramework/EntityRelations/ErExercise/ErExercise/Data/Models/Student.cs
@@ -8,6 +8,12 @@ namespace P01_StudentSystem.Data.Models
 {
     public class Student
     {
+        public Student()
+        {
+            this.HomeworkSubmissions = new HashSet<Homework>();
+            this.CourseEnrollments = new HashSet<StudentCourse>();
+        }
+
         public int StudentId { get; set; }
 
         [Required]
@@ -17,8 +23,13 @@ namespace P01_StudentSystem.Data.Models
         [Column(TypeName = "char(10)")]
         public string PhoneNumber { get; set; }
 
-        public DateTime RegisterOn { get; set; }
+        public DateTime RegisteredOn { get; set; }
 
-        public DateTime? Birthhday { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<Homework> HomeworkSubmissions { get; set; }
+
+        public ICollection<StudentCourse> CourseEnrollments { get; set; }
+
     }
 }
---
+++ EntityFramework/EntityRelations/ErExercise/P03_FootballBetting/Data/Models/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string Name{ get; set; }
+        public string LogoUrl { get; set; }
+        public string Initials { get; set; }
+        public decimal Budget { get; set; }
+
+        public int PrimaryKitColorId { get; set; }
+        public Color PrimaryKitColor { get; set; }
+        
+        public int SecondaryKitColorId { get; set; }
+        public Color SecondaryKitColor { get; set; }
+
+        public string TownId{ get; set; }
+        public Town Town { get; set; }
+
+    }
+}
