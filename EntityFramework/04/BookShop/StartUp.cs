739708cd053299d8fc6a2a9e864d2c25f5926945
Petyo Georgiev <calevra1807@abv.bg>
2021-11-11 20:06:15 +0200

    new Changes del nod

--- EntityFramework/04/BookShop/StartUp.cs
+++ EntityFramework/04/BookShop/StartUp.cs
@@ -16,7 +16,67 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(CountCopiesByAuthor(db));
+            Console.WriteLine(RemoveBooks(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(c => c.Copies < 4200)
+                .ToList();
+
+            context.Books.RemoveRange(books);
+            context.SaveChanges();
+            
+            return books.Count;
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(d => d.ReleaseDate.Value.Year < 2010)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var categoryBooks = context.Categories
+                .Select(category => new
+                {
+                    CatName = category.Name,
+                    Books = category.CategoryBooks.Select(b => new
+                    {
+                        b.Book.Title,
+                        b.Book.ReleaseDate.Value
+                    })
+                    .OrderByDescending(b => b.Value)
+                    .Take(3)
+                    .ToList()
+                })
+                .OrderBy(x => x.CatName)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var category in categoryBooks)
+            {
+                sb.AppendLine($"--{category.CatName}");
+
+                foreach (var book in category.Books)
+                {
+                    sb.AppendLine($"{book.Title} ({book.Value.Year})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string GetTotalProfitByCategory(BookShopContext context)
