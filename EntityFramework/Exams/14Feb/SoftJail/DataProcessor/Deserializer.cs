d9e78962ea37e91307662fab75a25b21092356a3
Petyo Georgiev <calevra1807@abv.bg>
2021-11-20 22:27:18 +0200

    NewReactDemAcad

--- EntityFramework/Exams/14Feb/SoftJail/DataProcessor/Deserializer.cs
+++ EntityFramework/Exams/14Feb/SoftJail/DataProcessor/Deserializer.cs
@@ -4,10 +4,12 @@
     using Data;
     using Newtonsoft.Json;
     using SoftJail.Data.Models;
+    using SoftJail.Data.Models.Enums;
     using SoftJail.DataProcessor.ImportDto;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
     using System.Linq;
     using System.Text;
 
@@ -24,7 +26,7 @@
             {
                 if (!IsValid(departmentCell) ||
                     !departmentCell.Cells.All(IsValid) ||
-                    !departmentCell.Cells.Any())
+                    departmentCell.Cells.Count == 0) // bateee
                 {
                     sb.AppendLine("Invalid Data");
                     continue;
@@ -44,6 +46,7 @@
                 departments.Add(department);
 
                 sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+                             // Imported {department name} with {cells count} cells
             }
 
             context.Departments.AddRange(departments);
@@ -54,12 +57,100 @@
 
         public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            var prisoners = new List<Prisoner>();
+
+            var prisonerMails = JsonConvert.DeserializeObject<IEnumerable<PrisonerMailInputModel>>(jsonString);
+
+            foreach (var currentPrisoner in prisonerMails)
+            {
+                if (!IsValid(currentPrisoner) ||
+                    !currentPrisoner.Mails.All(IsValid))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                // dd/MM/yyyy
+                var isValidReleaseDate = DateTime.TryParseExact(
+                    currentPrisoner.ReleaseDate,
+                    "dd/MM/yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out DateTime releaseDate);
+
+                var incarcerationDate = DateTime.ParseExact(
+                    currentPrisoner.IncarcerationDate,
+                    "dd/MM/yyyy",
+                    CultureInfo.InvariantCulture);
+
+                var prisoner = new Prisoner
+                {
+                    FullName = currentPrisoner.FullName,
+                    Nickname = currentPrisoner.Nickname,
+                    Age = currentPrisoner.Age,
+                    Bail = currentPrisoner.Bail,
+                    CellId = currentPrisoner.CellId,
+                    ReleaseDate = isValidReleaseDate ? (DateTime?)releaseDate : null,
+                    IncarcerationDate = incarcerationDate,
+                    Mails = currentPrisoner.Mails.Select(m => new Mail
+                    {
+                        Sender = m.Sender,
+                        Address = m.Address,
+                        Description = m.Description
+                    }).ToList()
+                };
+
+                prisoners.Add(prisoner);
+
+                sb.AppendLine($"Imported {prisoner.FullName} {prisoner.Age} years old");
+            }
+
+            context.Prisoners.AddRange(prisoners);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            var validOfficers = new List<Officer>();
+
+
+            var officerPrisoners = XmlConverter.Deserializer<OfficerPrisonerInputModel>(xmlString, "Officers");
+
+            foreach (var officerPrisoner in officerPrisoners)
+            {
+                if (!IsValid(officerPrisoner))
+                {
+                    sb.AppendLine($"Invalid Data");
+                    continue;
+                }
+
+                var officer = new Officer
+                {
+                    FullName = officerPrisoner.Name,
+                    Salary = officerPrisoner.Money,
+                    DepartmentId = officerPrisoner.DepartmentId,
+                    Position = Enum.Parse<Position>(officerPrisoner.Position),
+                    Weapon = Enum.Parse<Weapon>(officerPrisoner.Weapon),
+                    OfficerPrisoners = officerPrisoner.Prisoners.Select(x => new OfficerPrisoner
+                    {
+                        PrisonerId = x.Id
+                    })
+                    .ToList()
+                };
+
+                validOfficers.Add(officer);
+
+                sb.AppendLine($"Imported {officer.FullName} ({officer.OfficerPrisoners.Count} prisoners)");
+            }
+
+            context.Officers.AddRange(validOfficers);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         private static bool IsValid(object obj)
---
+++ ReactPractice/react-demo-acad/src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore React'/>
+    </div>
+  );
+}
+
+export default App;
